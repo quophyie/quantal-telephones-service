115fe580e9b8ad21ef5cc1e3d4598e04acca98ac
quophyie <quophyie@yahoo.com>
2019-03-07 22:28:35 +0000

    Updated libraries with invesdwin-instrument dependecy so that aspectj aspects are woven when running tests. Updates tests to allow aspectj aspects to be woven when running tests

--- src/main/java/com/quantal/telephones/config/api/ApiConfig.java
+++ src/main/java/com/quantal/telephones/config/api/ApiConfig.java
@@ -2,10 +2,12 @@ package com.quantal.telephones.config.api;
 
 import com.quantal.javashared.aspects.RetrofitRequiredHeadersEnforcerAspectJAspect;
 import com.quantal.javashared.constants.CommonConstants;
+import com.quantal.javashared.constants.Events;
 import com.quantal.javashared.dto.LogEvent;
 import com.quantal.javashared.dto.LoggerConfig;
 import com.quantal.javashared.logger.QuantalLogger;
 import com.quantal.javashared.logger.QuantalLoggerFactory;
+import com.quantal.javashared.util.CommonUtils;
 import com.quantal.telephones.services.api.GiphyApiService;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
@@ -15,9 +17,11 @@ import okio.Buffer;
 import org.aspectj.lang.Aspects;
 import org.slf4j.MDC;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
+import org.springframework.util.StringUtils;
 import retrofit2.Retrofit;
 import retrofit2.adapter.java8.Java8CallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
@@ -25,6 +29,7 @@ import retrofit2.converter.scalars.ScalarsConverterFactory;
 
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.List;
 
 import static com.quantal.javashared.constants.CommonConstants.EVENT_HEADER_KEY;
 import static com.quantal.javashared.constants.CommonConstants.EVENT_KEY;
@@ -42,6 +47,9 @@ public class ApiConfig
 {
     private final Environment env;
     private QuantalLogger logger;
+
+    @Value("${app.service-endpoints-not-requiring-mandatory-propagated-headers-uri-patterns}")
+    private List<String> serviceEndpointsNotRequiringMandatoryPropagatedHeadersPatterns;
 
     @Autowired
     public ApiConfig(Environment env, LoggerConfig loggerConfig){
@@ -75,6 +83,12 @@ public class ApiConfig
             MDC.put(EVENT_KEY, request.header(EVENT_HEADER_KEY));
             MDC.put(TRACE_ID_MDC_KEY, request.header(TRACE_ID_HEADER_KEY));
             LogEvent event = new LogEvent(request.header(CommonConstants.EVENT_HEADER_KEY));
+
+
+            boolean bEndpointRequiresMandatoryPropagatedHeaders = CommonUtils
+                    .isMandatoryPropagatedHeadersRequiredToCallEndpoint(request.url().toString(),
+                            serviceEndpointsNotRequiringMandatoryPropagatedHeadersPatterns);
+
             final boolean bAllRequiredHeadersFound = request
                     .headers()
                     .names()
@@ -82,7 +96,7 @@ public class ApiConfig
                     .filter(name -> name.equalsIgnoreCase(EVENT_HEADER_KEY) || name.equalsIgnoreCase(TRACE_ID_HEADER_KEY))
                     .count() == 2;
 
-            if(!bAllRequiredHeadersFound){
+            if(!bAllRequiredHeadersFound && bEndpointRequiresMandatoryPropagatedHeaders){
                 String msg = String.format("The required request headers %s and %s were not found. " +
                                 "Please make sure that your api interface method " +
                                 "has headers %s and %s ",
@@ -116,6 +130,22 @@ public class ApiConfig
             long t2 = System.nanoTime();
             String responseBody =response.body().string();
 
+            if (StringUtils.isEmpty(MDC.get(EVENT_KEY))
+                    && !StringUtils.isEmpty(request.header(EVENT_HEADER_KEY))){
+                MDC.put(EVENT_KEY, request.header(EVENT_HEADER_KEY));
+
+            }
+
+            if (StringUtils.isEmpty(MDC.get(TRACE_ID_MDC_KEY))
+                    && !StringUtils.isEmpty(request.header(TRACE_ID_HEADER_KEY))){
+                MDC.put(TRACE_ID_MDC_KEY, request.header(TRACE_ID_HEADER_KEY));
+
+            }
+
+            if (event.getEvent().equalsIgnoreCase(Events.DEFAULT_REQUEST_EVENT)){
+                event = new LogEvent(Events.DEFAULT_RESPONSE_EVENT);
+            }
+
             logger.info(String.format("Received response for %s",
                     response.request().url()),
                     new HashMap<String, Object>() {{
@@ -123,7 +153,7 @@ public class ApiConfig
                         put("headers", response.headers());
                         put("responseBody",responseBody);
                         put("statusCode",response.code());
-                    }});
+                    }}, event);
 
             return response.newBuilder()
                     .body(ResponseBody.create(response.body().contentType(), responseBody))
--- src/test/java/com/quantal/telephones/facades/TelephonesFacadeTest.java
+++ src/test/java/com/quantal/telephones/facades/TelephonesFacadeTest.java
@@ -1,27 +1,16 @@
 package com.quantal.telephones.facades;
 
-import com.quantal.javashared.util.TestUtil;
-import com.quantal.telephones.dto.TelephoneDto;
-import com.quantal.telephones.enums.TelephoneNumType;
-import com.quantal.telephones.models.Telephone;
 import com.quantal.telephones.services.api.GiphyApiService;
 import com.quantal.telephones.services.interfaces.TelephonesService;
-import org.assertj.core.api.Java6Assertions;
+import de.invesdwin.instrument.DynamicInstrumentationLoader;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.BDDMockito.given;
-import static org.mockito.Matchers.eq;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
 
 /**
  * Created by dman on 25/03/2017.
@@ -41,6 +30,13 @@ public class TelephonesFacadeTest {
     @Autowired
    // @InjectMocks
     private TelephonesFacade telephonesFacade;
+
+    @BeforeClass
+    public static void beforeClass(){
+        //Starts the aspectj weaver so that we can weave the compile time aspects
+        DynamicInstrumentationLoader.waitForInitialized(); //dynamically attach java agent to jvm if not already present
+        DynamicInstrumentationLoader.initLoadTimeWeavingContext(); //weave all classes before they are loaded as beans
+    }
 
     @Before
     public void setUp(){
