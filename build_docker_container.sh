f4846e6361480b32ae550021b31b7a0885b535ea
quophyie <quophyie@yahoo.com>
2019-03-18 23:57:34 +0000

    Updated Application entry point (i.e. QuantalTelephonesServiceApplication.java) with de.invesdwin.instrument.DynamicInstrumentationLoader Updated docker-compose yml with defaults for env vars WEB_APP_PORT and DB_PORT_EXPOSED_ON_HOST
    Added
     - script/build_run_docker_container.sh
     - script/variables.sh
    
     Deleted build_docker_container.sh

--- build_docker_container.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-. ~/.bash_profile
-docker run postgres_quantal-telephones-service & mvn package && docker-compose -f docker/compose/docker-compose.yml build --no-cache
--- src/main/java/com/quantal/telephones/QuantalTelephonesServiceApplication.java
+++ src/main/java/com/quantal/telephones/QuantalTelephonesServiceApplication.java
@@ -1,10 +1,17 @@
 package com.quantal.telephones;
 
+import de.invesdwin.instrument.DynamicInstrumentationLoader;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication(scanBasePackages = "com.quantal", scanBasePackageClasses = {com.quantal.telephones.config.shared.SharedConfig.class})
 public class QuantalTelephonesServiceApplication {
+
+	static {
+		//Starts the aspectj weaver so that we can weave the compile time aspects
+		DynamicInstrumentationLoader.waitForInitialized(); //dynamically attach java agent to jvm if not already present
+		DynamicInstrumentationLoader.initLoadTimeWeavingContext(); //weave all classes before they are loaded as beans
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(QuantalTelephonesServiceApplication.class, args);
